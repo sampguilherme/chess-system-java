facf365b6442cbe4d2e1d08a5781e92041ed02e9
Guilherme Sampaio <guilherme.sampaio324@hotmail.com>
2026-05-03 21:48:41 -0300

    Possible mopves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -45,6 +45,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source positio");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("The is no possible moves for the chosen piece");
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
